c8ed37d7f78b53f44977c2b74ca28ec931b275d7
yoyo-C <zorachen2010@gmail.com>
2015-09-25 00:55:51 +0800

    Type Conersion

---
+++ TypeConversion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class TypeConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int C, F;
+		F = in.nextInt();
+		C = (int)((F-32)*5/9);
+		System.out.println(C);
+	}
+
+}
