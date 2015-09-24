03ed385d678a1e13e0d2ce74e9d4d180cfa61e2b
yoyo-C <zorachen2010@gmail.com>
2015-09-25 00:48:59 +0800

    Sum of odd number and even number

---
+++ SumOfOddandEven.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class SumOfOddandEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int input;
+		int numOfEven = 0, numOfOdd = 0;
+		boolean flag = true;
+		while (flag){
+			input = in.nextInt();
+			if (input == -1){
+				break;
+			}
+			if (input % 2 == 0){
+				numOfEven++;
+			}
+			else{
+				numOfOdd++;
+			}
+		}
+		System.out.println(numOfEven+" "+ numOfOdd);	
+	}
+
+}
