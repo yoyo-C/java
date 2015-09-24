77837e529e04c74dc26a7fbcbaf168d4a8fc7fca
yoyo-C <zorachen2010@gmail.com>
2015-09-24 23:54:47 +0800

    LengthOfWord

---
+++ LenthOfWord.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class LenthOfWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		  
+		String word = in.next();
+		while(word.charAt(word.length() - 1) != '.'){
+			System.out.print(word.length()+" ");
+			word = in.next();
+		}
+		System.out.print(word.length() - 1); 
+	 } 
+}
