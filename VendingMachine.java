4f342ec93026ae3ababda4ddff0ab081adcba86a
yoyo-C <zorachen2010@gmail.com>
2015-10-09 23:42:44 +0800

    Update VendingMachine.java

--- VendingMachine.java
+++ VendingMachine.java
@@ -3,24 +3,42 @@ import java.util.Scanner;
 
 public class VendingMachine {
 
+	/**
+	 * @param args
+	 */
+	int price = 80;
+	int balance = 100;
+	int total;
+	int sum;
+
+	
+	void showPromt(){
+		System.out.println("Welcome");
+	}
+	
+	void insertMoney(int amount){
+		balance += amount;
+	}
+	
+	void showBalance(){
+		System.out.println(balance);
+	}
+	
+	void getFood(){
+		if (balance >= price){
+			System.out.println("Here you are.");
+			balance -= price;
+			total += price;
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
-		int balance = 0;
-		while(true){
-			System.out.print("请投币：");
-			int amount = in.nextInt();
-			balance += amount;
-			if (balance >= 10){
-				System.out.println("************");
-				System.out.println("Java城际铁路专线");
-				System.out.println("  无指定座位票       ");
-				System.out.println("  票价：10元         ");
-				System.out.println("************");
-				System.out.println("找零："+(balance-10));
-				balance = 0;
-			}
-		}
+		VendingMachine vm = new VendingMachine();
+		vm.showPromt();
+		vm.showBalance();
+		vm.insertMoney(100);
+		vm.getFood();
+		vm.showBalance();
 	}
 
 }
